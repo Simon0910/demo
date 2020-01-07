9c67c57f5d8689e1877475b8953544a44c12004f
liuzhipeng <xnliuzhipeng@jd.com>
2020-01-07 18:31:48 +0800

    fix

--- jdk/src/main/java/lang/Demo.java
+++ jdk/src/main/java/lang/Demo.java
@@ -1,5 +1,8 @@
 package lang;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author liuzhipeng
  * @description
@@ -9,5 +12,11 @@ public class Demo {
     public static void main(String[] args) {
         System.out.printf("Start TransmittableThreadLocal[%s] Dump...\n", "title1");
         System.out.format("Start TransmittableThreadLocal[%s] Dump...\n", "title2");
+        Map<String, Object> map = new HashMap<>(2000);
+        for (int i = 0; i < 1000; i++) {
+            map.put("collectUniqueNo", "value1234567");
+        }
+        System.out.println("ok...");
+
     }
 }
