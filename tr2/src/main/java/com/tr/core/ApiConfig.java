9caddb4fcfc6072122d9cae6dd82f0ec8d1e2a09
liuzhipeng <xnliuzhipeng@jd.com>
2020-01-09 17:57:45 +0800

    fix

--- tr2/src/main/java/com/tr/core/ApiConfig.java
+++ tr2/src/main/java/com/tr/core/ApiConfig.java
@@ -10,7 +10,8 @@ public class ApiConfig {
         Configuration configuration = new Configuration();
         configuration.setIsEnable(true);
         configuration.setDefaultKeyStorePassword("123456");
-        configuration.setDefaultKeyStorePath("E:/JD_RES/https/CJD_DolfinWallet_XMLGW_MTF_160680/keystore.jks");
+        // configuration.setDefaultKeyStorePath("E:/JD_RES/https/CJD_DolfinWallet_XMLGW_MTF_160680/keystore.jks");
+        configuration.setDefaultKeyStorePath("E:\\github\\demo\\tr2\\src\\main\\resources\\jd-mtf-mastercard-keystore.jks");
         configuration.enableSSL();
         new com.tr.core.encrypt.config.Configuration();
         new com.tr.core.oauth1.config.Configuration();
