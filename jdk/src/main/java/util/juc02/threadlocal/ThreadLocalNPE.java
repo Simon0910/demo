5849fba22d6ab4e44aaac1b564fa0515018c21fb
lzp <15321168632@163.com>
2020-06-21 20:58:39 +0800

    juc02

---
+++ jdk/src/main/java/util/juc02/threadlocal/ThreadLocalNPE.java
@@ -0,0 +1,31 @@
+package util.juc02.threadlocal;
+
+/**
+ * 描述：     TODO
+ */
+public class ThreadLocalNPE {
+
+    ThreadLocal<Long> longThreadLocal = new ThreadLocal<Long>();
+
+    public void set() {
+        longThreadLocal.set(Thread.currentThread().getId());
+    }
+
+    // public long get() {
+    public Long get() {
+        return longThreadLocal.get();
+    }
+
+    public static void main(String[] args) {
+        ThreadLocalNPE threadLocalNPE = new ThreadLocalNPE();
+        System.out.println(threadLocalNPE.get());
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                threadLocalNPE.set();
+                System.out.println(threadLocalNPE.get());
+            }
+        });
+        thread1.start();
+    }
+}
